51c7b5c21fe9e75ef6aa72dcbfd7d1e6d845cdb5
modmuss50 <modmuss50@gmail.com>
2017-03-06 22:06:36 +0000

    Add basic discord support

--- Main.go
+++ Main.go
@@ -3,13 +3,68 @@ package main
 import (
 	"fmt"
 	"github.com/modmuss50/discordBot/minecraft"
+	"github.com/bwmarrin/discordgo"
+	"io/ioutil"
+)
+
+var (
+	Token string
+	BotID string
 )
 
 func main() {
+	LoadDiscord()
+}
 
-	var latest = minecraft.GetLatest()
-	fmt.Println("Latest snapshot: " + latest.Snapshot)
-	fmt.Println("Latest release: " + latest.Release)
+//Based a lot off https://github.com/bwmarrin/discordgo/blob/master/examples/pingpong/main.go
+func LoadDiscord() {
+
+	Token = getToken()
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	u, err := dg.User("@me")
+	if err != nil {
+		fmt.Println("error obtaining account details,", err)
+	}
+	BotID = u.ID
+
+	dg.AddHandler(handleMessage)
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	<-make(chan struct{})
+	return
+}
+
+//Called when a message is posted
+func handleMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
+	fmt.Println(m.Author.Username + ":" + m.Content)
+	if m.Author.ID == BotID {
+		return
+	}
+	if m.Content == "!version" {
+		var latest = minecraft.GetLatest()
+		_, _ = s.ChannelMessageSend(m.ChannelID, "Latest snapshot: " + latest.Snapshot)
+		_, _ = s.ChannelMessageSend(m.ChannelID, "Latest release: " + latest.Release)
+	}
+}
+
+//Loads the token from the file
+func getToken() string {
+	b, err := ioutil.ReadFile("token.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	return string(b)
 }
 
 
